7869ce6c835c5c404794cb690ed2b0fd8f21ba5b
ankitmishra8652 <ankitmishra8652@gmail.com>
2023-08-23 01:21:54 +0530

    added a new feature of email summarization

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
-from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.probability import FreqDist
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 ps = PorterStemmer()
+
+# Function to transform and preprocess text
 
 
 def transform_text(text):
@@ -33,23 +38,53 @@ def transform_text(text):
     return " ".join(y)
 
 
+# Load the trained model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
+
+# TextRank-based extractive summarization
+
+
+def summarize_text(text, num_sentences=3):
+    sentences = sent_tokenize(text)
+    words = word_tokenize(text)
+
+    words = [word for word in words if word.lower(
+    ) not in stopwords.words('english')]
+
+    fdist = FreqDist(words)
+
+    sentence_scores = {}
+    for sentence in sentences:
+        for word in word_tokenize(sentence.lower()):
+            if word in fdist:
+                if len(sentence.split()) < 30:
+                    if sentence not in sentence_scores:
+                        sentence_scores[sentence] = fdist[word]
+                    else:
+                        sentence_scores[sentence] += fdist[word]
+
+    selected_sentences = sorted(
+        sentence_scores, key=sentence_scores.get, reverse=True)[:num_sentences]
+    summarized_text = TreebankWordDetokenizer().detokenize(selected_sentences)
+    return summarized_text
+
 
 st.title("Spam Shield : Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
         st.header(" Not a Spam")
+
+if st.button('Summarize'):
+    if input_sms:
+        summarized_text = summarize_text(input_sms)
+        st.write("Summarized Text:")
+        st.write(summarized_text)
